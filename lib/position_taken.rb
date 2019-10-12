713ea1b09baf4c702dffc0153eb5686d9f076f2c
Nicholas Ng <nicholasykng@gmail.com>
2019-10-12 16:35:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
 end
 end
