06b0f35f113e842e7b98f8d06eeedafa2b5d0b67
Nicholas Ng <nicholasykng@gmail.com>
2019-10-12 16:37:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-  if board[index] == " "
-    return false
-  elsif board[index] == ""
+  if board[index] == " " || board[index] == ""
     return false
 end
 end
